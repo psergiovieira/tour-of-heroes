b58f1dd465c98282a562985d13fa420637015c18
Paulo Sérgio Vieira <paulosv@cit.local>
2018-04-27 11:22:27 -0300

    creating a route to the application

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,13 +10,12 @@ import { HeroService } from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
+  heroes: Hero[];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
-  heroes: Hero[];
-
+  
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
@@ -26,4 +25,5 @@ export class HeroesComponent implements OnInit {
   getHeroes():void{
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
+
 }
