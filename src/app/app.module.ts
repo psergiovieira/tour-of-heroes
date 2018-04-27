9c0b9cd6588066eec2ae25b11295c1d8e5f68bc2
Paulo Sérgio Vieira <paulosv@cit.local>
2018-04-27 10:01:25 -0300

    Adding and using my first service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'; 
+import {HeroService} from './hero.service';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
