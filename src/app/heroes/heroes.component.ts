0d0f21a15287ae18787dca84ef34a54561f0f22a
Paulo Sérgio Vieira <paulosv@cit.local>
2018-04-27 15:18:19 -0300

    routing and styling the application

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,14 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-  heroes: Hero[];
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-  
+  heroes: Hero[];  
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
