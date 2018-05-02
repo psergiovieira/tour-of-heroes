4d2f18a9f173ed86ed6edd9d50744cf031d79e4b
Paulo Sérgio Vieira <paulosv@cit.local>
2018-05-02 09:28:24 -0300

    handler errors

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable()
 export class HeroService {
@@ -14,7 +15,9 @@ export class HeroService {
 
 
   getHeroes (): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl)
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      catchError(this.handleError('getHeroes',[]))
+    );
   }
 
   getHero(id: number): Observable<Hero>{    
@@ -26,5 +29,25 @@ export class HeroService {
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add('HeroService: ' + message);
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+    
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
